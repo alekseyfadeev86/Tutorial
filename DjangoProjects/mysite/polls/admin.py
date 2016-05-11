7c0aba47ab35a3ec84c9432ef347427b4212e5d7
Aleksey Fadeev <fadeev.alexey@alphaopen.com>
2016-05-11 10:09:17 +0300

    -4-

--- DjangoProjects/mysite/polls/admin.py
+++ DjangoProjects/mysite/polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+
+class QuestionAdmin( admin.ModelAdmin ):
+    #fields = [ 'pub_date', 'question_text' ]
+    fieldsets = [
+        ( None,               { 'fields' : [ 'question_text' ] } ),
+        ( 'Date information', { 'fields' : [ 'pub_date' ], 'classes' : [ 'collapse' ] }),
+    ]
+
+admin.site.register( Question, QuestionAdmin )
+admin.site.register( Choice )
